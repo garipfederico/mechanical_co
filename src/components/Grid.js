0eca51489b1ff2a0eb737bcb110aede993b87104
garipfederico <garip.federico@gmail.com>
2022-03-11 03:58:21 -0300

    Fix_secondsToUpdate_preset
    Fix: preseted values from 300 to 15 fixed

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,4 +1,4 @@
-import Card from './CardGeneric'
+import CardGeneric from './CardGeneric'
 
 export default function Cards() {
     // SET VARIABLES FROM HERE
@@ -9,13 +9,14 @@ export default function Cards() {
         "users", "workflows"]
 
     // Time to refresh/update the info from server (in seconds)
-    const secondsToUpdate = 300
+    const secondsToUpdate = 15
 
     return (
         // <div className="d-flex align-content-start justify-content-start flex-wrap">
+        //This will create a grid component and then for each name in the API_Names will create a 
         <div className="row row-cols-1 row-cols-sm-2 row-cols-md-4 row-cols-lg-6 row-cols-xl-8 g-4 mx-2 p-4 ">
             {API_Names.map((names, index) =>
-            <Card   name={names}
+            <CardGeneric   name={names}
                     secondsToUpdate={secondsToUpdate}
                     order={index}
                     key={index} />)}
